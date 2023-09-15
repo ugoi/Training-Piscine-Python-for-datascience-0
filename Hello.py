65631b6d752ed10abe6ff0f91ac549ac26752e3e
Stefan Dukic <96357300+UGOI@users.noreply.github.com>
2023-09-15 23:28:35 +0200

    initial commit

---
+++ Hello.py
@@ -0,0 +1,21 @@
+ft_list  = ["Hello", "tata!"]
+ft_tuple = ("Hello", "toto!")
+ft_set   = {"Hello", "tutu!"}
+ft_dict  = {"Hello" : "titi!"}
+
+#your code here
+ft_list[1] = "World"
+
+ft_tuple_lst = list(ft_tuple)
+ft_tuple_lst[1] = "Germany"
+ft_tuple = tuple(ft_tuple_lst)
+
+ft_set.remove("tutu!")
+ft_set.add("Heilbronn")
+ft_dict["Hello"] = "42Heilbronn"
+
+
+print(ft_list)
+print(ft_tuple)
+print(ft_set)
+print(ft_dict)
