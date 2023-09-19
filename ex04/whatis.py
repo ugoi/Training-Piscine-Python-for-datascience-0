89d79705ac4c4940c897d9378f1bd6569cabd8d2
Stefan Dukic <96357300+UGOI@users.noreply.github.com>
2023-09-19 13:08:02 +0200

    test(ex04): make it testable and add tests

--- ex04/whatis.py
+++ ex04/whatis.py
@@ -4,17 +4,17 @@ def whatis():
     # total arguments
     n = len(sys.argv)
     if (n == 1):
-        sys.exit(0)
+        return
     elif (n != 2):
         print("AssertionError: more than one argument is provided")
-        sys.exit(0)
+        return
 
     
     try:
         number: int = int(sys.argv[1])
     except ValueError:
         print("AssertionError: argument is not an integer")
-        sys.exit(1)
+        return
 
     if (number % 2):
         print("I'm Odd.")
