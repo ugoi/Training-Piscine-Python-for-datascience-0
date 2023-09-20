72cf2ee2cbacb80590e18601923587c4b9dac7bf
Stefan Dukic <96357300+UGOI@users.noreply.github.com>
2023-09-20 15:34:40 +0200

    fix(ex06): list comprehension
    use list comprehension isntead of enerator expression in ft_filter

--- ex06/ft_filter.py
+++ ex06/ft_filter.py
@@ -5,4 +5,4 @@ filter(function or None, iterable) --> filter object
 Return an iterator yielding those items of iterable for which function(item)
 is true. If function is None, return the items that are true.
     """
-    return (item for item in iterable if function(item))
+    return [item for item in iterable if function(item)]
