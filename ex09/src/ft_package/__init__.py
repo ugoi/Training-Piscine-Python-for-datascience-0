be6232879cdf809286cc432b2a4e3935cda6da4d
Stefan Dukic <96357300+UGOI@users.noreply.github.com>
2023-09-21 20:52:33 +0200

    feat(ex09): solution + test
    this is the last exercise

---
+++ ex09/src/ft_package/__init__.py
@@ -0,0 +1,1 @@
+from .example import count_in_list  # noqa: F401
---
+++ ex09/src/ft_package/example.py
@@ -0,0 +1,10 @@
+def count_in_list(list: list, string: str) -> int:
+    """
+    Count occurrences of a term in a list.
+    Args:
+    - lst (List): The list to search in.
+    - term: The term to search for.
+    Returns:
+    int: Number of occurrences of the term in the list.
+    """
+    return list.count(string)
